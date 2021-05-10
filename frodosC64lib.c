6e12fcfabb77e231e90bbc1bf9fe4efe239db670
Wil <wilfried@gmx.at>
2021-05-10 21:26:31 +0200

    updated library

--- frodosC64lib.c
+++ frodosC64lib.c
@@ -1,6 +1,6 @@
 /********************************************** 
  * Frodo's C64 library 
- * Jul-2016 V1.0
+ * May-2021 V1.4
  * Wilfried Elmenreich
  * Code under CC-BY-4.0 license
  * made as part of #bitbitjam3
@@ -8,20 +8,30 @@
 
 #include "frodosC64lib.h"
 
-const unsigned char twoponentials[] =
+const unsigned char twopotentials[] =
 {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
 static unsigned char globalSubN, globalSubY;
 static unsigned int globalSubX;
-//static unsigned long cnt;
+
+/* Wait delay in ms */
+void delayms(int delay)
+{
+    globalSubX = delay;
+    __asm__ ("ldx %v",globalSubX);
+    __asm__ ("lda %v+1",globalSubX);
+    __asm__ ("dllp0: ldy #$b6");
+    __asm__ ("dllp1: dey");
+    __asm__ ("bne dllp1");
+    __asm__ ("dex");
+    __asm__ ("bne dllp0");
+    __asm__ ("sec");
+    __asm__ ("sbc #$01");
+    __asm__ ("bpl dllp0");
+}
 
 /* n being the sprite number (0..7) */
 void setSpriteXY(char n,int x,char y)
 {
-//     cnt+=1;
-//     if (cnt % 100 == 0) {
-//        gotoxy(0,10);
-//        cprintf("%d %d",cnt,clock());
-//     }
      globalSubN = n;
      globalSubX = x;
      globalSubY = y;
@@ -29,32 +39,43 @@ void setSpriteXY(char n,int x,char y)
      __asm__ ("tay");
      __asm__ ("asl");
      __asm__ ("tax");
-     __asm__ ("lda %v,y", twoponentials);
+     __asm__ ("lda %v,y", twopotentials);
      __asm__ ("tay");
      __asm__ ("lda %v", globalSubY);
-     __asm__ ("sta 53249,x"); //set y coordinate
+     __asm__ ("sta %w,x", VIC_BASE+1); //set y coordinate
      __asm__ ("lda %v", globalSubX); //load low value of x coordinate
-     __asm__ ("sta 53248,x"); //set x coordinateb(lower byte)
+     __asm__ ("sta %w,x", VIC_BASE);  //set x coordinate (lower byte)
      __asm__ ("tya");
      __asm__ ("ldx %v+1", globalSubX); //load high value of x coordinate     
      __asm__ ("beq _label1");
-     __asm__ ("ora 53264");
+     __asm__ ("ora %w",VIC_BASE+0x10);
      __asm__ ("bne _fin");
      __asm__ ("_label1:");
      __asm__ ("eor #$FF");
-     __asm__ ("and 53264");
+     __asm__ ("and %w",VIC_BASE+0x10);
      __asm__ ("_fin:");
-     __asm__ ("sta 53264"); 
+     __asm__ ("sta %w",VIC_BASE+0x10);
 }
 
-void setSpriteXY2(char n,int x,char y)
-{
-     poke(53248u+n+n+1,y);
-     poke(53248u+n+n,x);
-     if (x>255)
-          poke(53264u,peek(53264u)|(1<<n));
-     else
-          poke(53264u,peek(53264u)&(0xFF - (1<<n)));     
+void updateSpriteAttributes(char n) {
+    globalSubN=peek(SCREEN_BASE+0x3f8+n);
+    
+     __asm__ ("sei");
+     __asm__ ("lda $1");
+     __asm__ ("pha");
+     __asm__ ("lda #$34"); //make RAM visible in all areas       
+     __asm__ ("sta $1");     
+     globalSubX=peek(VIC_BANK + 0x3f + 64*globalSubN);
+     __asm__ ("pla");     
+     __asm__ ("sta $1");              
+     __asm__ ("cli");     
+     
+    if (globalSubX>=128) {
+      enableMultiColorSprite(n);
+    } else {
+      disableMultiColorSprite(n);
+    }     
+    setSpriteColor(n,globalSubX);     
 }
 
 void blinkwait(unsigned int seconds,unsigned char color)
@@ -68,22 +89,85 @@ void blinkwait(unsigned int seconds,unsigned char color)
 }
 
 /* bank index from 0..3 
-  * 3 $0000–$3FFF Char ROM at $1000-$1FFF
-  * 2 $4000–$7FFF Char ROM inactive
-  * 1 $8000–$BFFF Char ROM at $9000–$9FFF
-  * 0 $C000–$FFFF Char ROM inactive **/
+  * 0 $0000–$3FFF Char ROM at $1000-$1FFF
+  * 1 $4000–$7FFF Char ROM inactive
+  * 2 $8000–$BFFF Char ROM at $9000–$9FFF
+  * 3 $C000–$FFFF Char ROM inactive **/
 void setVICbank(char bank)
 {
-     poke(0xDD00, peek(0xDD00) & 0xFC | bank);
+     poke(0xDD00, peek(0xDD00) & 0xFC | (3-bank));
 }
 
-void setVICcharset(char index)
-{
-     poke(53272U,(peek(53272U) & 240) | (index*2));
+//void setVICcharset(char index)
+//{
+     //poke(53272U,(peek(53272U) & 240) | (index*2));
+//}
+
+//void setVICscreen(unsigned int index)
+//{
+     //poke(0xD018, peek(0xD018) & 0x0F | (index*16)); //set VIC
+     //poke(648,index*4+(3-peek(0xDD00)&3)*64); //tell the OS where to print
+//}
+
+
+int getkey() {
+    int pressedkey=0;
+    poke(198,0);
+    do
+    {
+      pressedkey=get();
+    }
+    while (pressedkey==0);
+    return pressedkey;
 }
 
-void setVICscreen(unsigned int index)
+unsigned int input_uint()
 {
-     poke(0xD018, peek(0xD018) & 0x0F | (index*16)); //set VIC
-     poke(648,index*4+(3-peek(0xDD00)&3)*64); //tell the OS where to print
+     unsigned int v=0;
+     int digits=0,pressedkey;
+     
+     cputc(175); //cursor
+     asm("lda #$9D");     
+     asm("jsr $FFD2"); //cursor left
+//     cputc(157); //left
+     while(1) {
+       pressedkey=getkey();
+       if (pressedkey==13) break;
+       if ((pressedkey>='0')&&(pressedkey<='9')&&digits<5) {
+         v=v*10+pressedkey-'0';
+         cputc(pressedkey);
+         cputc(175); //cursor
+         asm("lda #$9D");     
+         asm("jsr $FFD2"); //cursor left
+         digits++;
+       }
+       if ((pressedkey==20) && (digits>0)) {
+         v=v/10;
+         asm("lda #$14");     
+         asm("jsr $FFD2"); //cursor left 
+         digits--;       
+       }
+     }
+     cputc(' '); //delete cursor
+     return v;
 }
+
+void wait_end_of_frame(void)
+{
+  //wait for raster < 255
+  __asm__ ("wtlp00: lda $D011");
+  __asm__ ("and #$80");
+  __asm__ ("bne wtlp00");
+  //wait for raster > 255  
+   __asm__ ("wtlp01: lda $D011");
+  __asm__ ("and #$80");
+  __asm__ ("beq wtlp01"); 
+}
+
+void locate_cursor(uint8_t x,uint8_t y)
+{
+   poke(211,x);
+   poke(214,y);
+   asm("jsr 58640");
+}
+
--- frodosC64lib.h
+++ frodosC64lib.h
@@ -1,37 +1,82 @@
 /********************************************** 
  * Frodo's C64 library 
- * Jul-2016 V1.0
+ * May-2021 V1.4
  * Wilfried Elmenreich
  * Code under CC-BY-4.0 license
  **********************************************/
 
-#ifndef FRODOSC64LIB
-#define FRODOSC64LIB
+#ifndef _FRODOSC64LIB_H_
+#define _FRODOSC64LIB_H_
+
+#include <conio.h>
+#include <stdint.h>
+
+#include "../src/presets.h"
+
+#ifndef VIC_BASE
+  #define VIC_BASE 0xd000
+#endif
+
+#ifndef SCREEN_BASE
+  #define SCREEN_BASE 0x0400
+#endif
+
+#ifndef VIC_BANK
+  #define VIC_BANK 0x0000
+#endif
+
+
+
+/*IO functions*/
+#define get() (asm("jsr $FFE4"),asm("ldx #$00"),__AX__)
+#define textcolor(c)	(poke(646,c))
 
 /*general functions for memory access and interrupt control */
 #define poke(addr,val)	(*(unsigned char *)(addr)=(val))
 #define peek(addr) (*(unsigned char *)(addr))
-//#define disable_int poke(56334u, peek(56334u) & 254)  //deactivate interrupts
-//#define enable_int poke(56334u, peek(56334u) | 1)  //activate interrupts
-#define disable_int __asm__ ("sei")
-#define enable_int __asm__ ("cli")
-
+#define disable_int poke(56334u, peek(56334u) & 254)  //deactivate interrupts
+#define enable_int poke(56334u, peek(56334u) | 1)  //activate interrupts
 void setVICbank(char bank);
-void setVICcharset(char index);
-void setVICscreen(unsigned int index);
-#define random8 (peek(56324u)) //using low value of CIA timer as true random number generator
+
+#define setVICcharset(addr) (asm("lda %w+24",VIC_BASE),asm("and #$f0"),asm("ora #%b",(addr & 0x3800)/0x400),asm("sta %w+24",VIC_BASE))
+#define setVICscreen(addr) (asm("lda %w+24",VIC_BASE),asm("and #$0f"),asm("ora #%b",(addr & 0x3c00)/0x40),asm("sta %w+24",VIC_BASE))
+
+#define random8() (peek(56324u)) //using low value of CIA timer as true random number generator
+#define random16() (asm("lda $DC04"),asm("ldx $DC04"),__AX__)
+#define random15() (asm("lda $DC04"),asm("and #$7F"),asm("tax"),asm("lda $DC04"),__AX__)
+#define random(n) (random15() % (n))
 
 /* sprite functions */
-#define showSprite(n) (poke(53269u,peek(53269u)|(1<<n))) //activate sprite n
-#define hideSprite(n) (poke(53269u,peek(53269u)&(0xFF - (1<<n)))) //activate sprite n
-#define setSpriteColor(n,c) (poke(53287u+n,c))
-#define setSpriteMultiColor1(c) (poke(53285u,c))
-#define setSpriteMultiColor2(c) (poke(53286u,c))
+#define showSprite(n) (poke(VIC_BASE+21,peek(VIC_BASE+21)|(1<<n))) //activate sprite n
+#define hideSprite(n) (poke(VIC_BASE+21,peek(VIC_BASE+21)&(0xFF - (1<<n)))) //activate sprite n
+
+#define enableMultiColorSprite(n) (poke(VIC_BASE+0x1c,peek(VIC_BASE+0x1c)|(1<<n))) 
+#define disableMultiColorSprite(n) (poke(VIC_BASE+0x1c,peek(VIC_BASE+0x1c)&(0xFF - (1<<n))))
+
+#define enableXexpandSprite(n) (poke(VIC_BASE+0x1d,peek(VIC_BASE+0x1d)|(1<<n))) 
+#define disableXexpandSprite(n) (poke(VIC_BASE+0x1d,peek(VIC_BASE+0x1d)&(0xFF - (1<<n))))
+
+#define enableYexpandSprite(n) (poke(VIC_BASE+0x17,peek(VIC_BASE+0x17)|(1<<n))) 
+#define disableYexpandSprite(n) (poke(VIC_BASE+0x17,peek(VIC_BASE+0x17)&(0xFF - (1<<n))))
+
+#define setSpriteColor(n,c) (poke(VIC_BASE+0x27+n,c))
+#define setSpriteMultiColor1(c) (poke(VIC_BASE+0x25,c))
+#define setSpriteMultiColor2(c) (poke(VIC_BASE+0x26,c))
+#define setSpriteCostume(n,c) (*(unsigned char *)(SCREEN_BASE+0x3f8+n)=(c))
+
+#define getSpriteCostume(n) (peek(SCREEN_BASE+0x3f8+n))
+
+#define getSpriteXpos(n) ( peek(VIC_BASE+n+n)+((peek(VIC_BASE+16)&(1<<n))<<(8-n)) )
+#define getSpriteYpos(n) ( peek(VIC_BASE+1+n+n) )
+
 void setSpriteXY(char n,int x,char y);
+void updateSpriteAttributes(char n);
 
 /* character functions */
-#define setCharacterMultiColor poke(53270u,peek(53270u) | 16)
-#define setCharacterSingleColor poke(53270u,peek(53270u) & 239)
+#define setCharacterMultiColor poke(VIC_BASE+0x16,peek(VIC_BASE+0x16) | 16)
+#define setCharacterSingleColor poke(VIC_BASE+0x16,peek(VIC_BASE+0x16) & 239)
+#define setCharMultiColor1(c) (poke(VIC_BASE+0x22,c))
+#define setCharMultiColor2(c) (poke(VIC_BASE+0x23,c))
 
 /* joystick functions */
 #define joystick_up(n) ((*(unsigned char *)(56322u-n) & 0x01)==0)
@@ -42,5 +87,18 @@ void setSpriteXY(char n,int x,char y);
 
 /* misc functions */
 void blinkwait(unsigned int seconds,unsigned char color);
+void delayms(int delay);
+void wait_end_of_frame();
+int getkey();
+void locate_cursor(uint8_t x,uint8_t y);
+
+#define wait_for_rasterline(L) (__AX__ = (L),   \
+                         asm("@WTLP_%s:", __LINE__),     \
+                         asm("cmp $D012"),\
+                         asm("bne @WTLP_%s", __LINE__),  \
+                         asm("bit $D011"),\
+                         asm("bmi @WTLP_%s", __LINE__)  )
+
+extern const unsigned char twopotentials[];
 
 #endif
